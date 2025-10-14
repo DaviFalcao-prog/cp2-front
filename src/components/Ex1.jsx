d1fd0a5c626bf64fdad7cb2474051296a8616d27
DaviFalcao-prog <davifalcao1609outlook.com>
2025-10-14 19:44:15 -0300

    ex1-feito

--- src/components/Ex1.jsx
+++ src/components/Ex1.jsx
@@ -7,6 +7,9 @@ export default function Ex1() {
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex1 - Botão Estilizado</h1>
       {/* Resolva aqui */}
+      <div className="flex justify-center">
+        <button className="p-3 bg-blue-500 hover:bg-blue-600 transition-color text-white rounded">Botão</button>
+      </div>
     </div>
   );
 }
