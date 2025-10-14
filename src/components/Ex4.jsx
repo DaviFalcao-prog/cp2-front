3740ea7663327ea9653ea1e798b3341cc32d1a66
DaviFalcao-prog <davifalcao1609outlook.com>
2025-10-14 20:09:07 -0300

    ex4-complete

--- src/components/Ex4.jsx
+++ src/components/Ex4.jsx
@@ -7,6 +7,14 @@ export default function Ex4() {
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex4 - Card Responsivo</h1>
       {/* Resolva aqui */}
+      <div className="flex justify-center">
+        <div className="w-full md:w-[24rem] bg-gray-500 g-4 p-4 rounded">
+          <h1>Card Responsivo</h1>
+          <p>Texto inserido no card responsivo</p>
+          <button className="p-3 bg-blue-500 hover:bg-blue-600 transition-color text-white rounded">Botão</button>
+        </div>
+
+      </div>
     </div>
   );
 }
