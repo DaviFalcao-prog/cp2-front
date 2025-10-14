3c163964fb10aff8bfa144f9be126ef125d308c1
DaviFalcao-prog <davifalcao1609outlook.com>
2025-10-14 19:57:44 -0300

    ex3-completo

--- src/components/Ex3.jsx
+++ src/components/Ex3.jsx
@@ -6,6 +6,12 @@ export default function Ex3() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex3 - Grid Simples</h1>
+        <div className="grid grid-cols-2 grid-rows-2 gap-1">
+          <div className="bg-gray-500 h-[80px]"></div>
+          <div className="bg-gray-500 h-[80px]"></div>
+          <div className="bg-gray-500 h-[80px]"></div>
+          <div className="bg-gray-500 h-[80px]"></div>
+        </div>
       {/* Resolva aqui */}
     </div>
   );
