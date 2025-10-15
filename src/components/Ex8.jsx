ddd95b685a9bc785eb0da31861b82254bd02b13e
DaviFalcao-prog <davifalcao1609outlook.com>
2025-10-14 21:03:29 -0300

    ex8-complete

--- src/components/Ex8.jsx
+++ src/components/Ex8.jsx
@@ -13,10 +13,12 @@ export default function Ex8() {
   return (
     <>
     <h1 className="text-2xl font-bold mb-4">Ex8 - Card de Produto</h1>
-    <section className="flex justify-center items-center mb-12">
-      <div className="bg-white p-6 rounded-lg shadow-md w-64 text-center">
+    <section className="flex justify-center items-center mb-12 hover:scale-105 ">
+      <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-[5px_5px_5px] w-64 text-center">
         <div className="text-4xl text-blue-600 mb-4 flex justify-center">
-          <FaShoppingCart />
+          <div className="animate-bounce">
+            <FaShoppingCart />
+          </div>
         </div>
         <h3 className="text-lg font-semibold mb-2">Produto em Promoção</h3>
         <p className="text-gray-600 mb-4">Aproveite 20% de desconto!</p>
